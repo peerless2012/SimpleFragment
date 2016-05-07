3892bd6cc9d313c5788cd1f7deec88e10b5f1a11
peerless2012 <peerless2012@126.com>
2016-05-07 10:42:35 +0800

    同上

---
+++ src/com/peerless2012/simplefragment/fragments/TabFragment4.java
@@ -0,0 +1,42 @@
+package com.peerless2012.simplefragment.fragments;
+
+import com.peerless2012.simplefragment.R;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class TabFragment4 extends BaseFragment {
+	public final static String TAG = "TabFragment4";
+	public final static String CONTENT = "content";
+	private View rootView;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		//此方法介绍参照README.MD
+//		rootView = inflater.inflate(R.layout.fragment_tab, container, false);
+		rootView = inflater.inflate(R.layout.fragment_tab, null);
+		TextView content = (TextView) rootView.findViewById(R.id.content);
+		
+		rootView.setBackgroundColor(Color.GRAY);
+		Bundle data = getArguments();
+		if (data != null) {
+			content.setText(data.getString(CONTENT));
+		}
+		return rootView;
+	}
+
+	@Override
+	public void updateData(Bundle bundle) {
+		
+	}
+}
